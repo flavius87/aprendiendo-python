8338c1cdb9d7f17cbf528d54b0766b856eff7f02
flavius87 <flavio.scheck@gmail.com>
2021-01-11 22:26:23 -0300

    agregado man

--- 20-proyecto-python/usuarios/acciones.py
+++ 20-proyecto-python/usuarios/acciones.py
@@ -3,7 +3,7 @@ class Acciones:
     def registro(self):
         print("\nOk, vamos a registrarte en el sistema...") 
 
-        nombre = input("¿Cuál es tu nombre? ")
+        nombre = input("¿Cuál es tu nombre man? ")
         apellidos = input("¿Cuáles son tus apellidos? ")
         email = input("Introduce tu email ")
         password = input("Elige una contraseña ")
